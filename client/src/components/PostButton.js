5933fe2eb6e7ae7af6d934f0de0b19fb4c05cc4a
noellerjd <noellerninja@gmail.com>
2022-11-13 11:28:20 -0700

    updated button

--- client/src/components/PostButton.js
+++ client/src/components/PostButton.js
@@ -1,6 +1,9 @@
 const PostButton = () => {
   return (
-    <button className="w-full text-white bg-lightblue text-xl py-2 rounded-md">
+    <button
+      id="create-post-btn"
+      className="w-full text-white bg-lightblue text-xl py-2 rounded-md"
+    >
       Create a Post!
     </button>
   );
