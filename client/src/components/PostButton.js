1b026be352a9146ed0d1b28ab425b570171be59c
dpvb <bungohdev@gmail.com>
2022-11-13 23:44:44 -0500

    Created alternative post location.

--- client/src/components/PostButton.js
+++
@@ -1,9 +0,0 @@
-const PostButton = () => {
-  return (
-    <button className="transition ease-in-out duration-300 text-white bg-lightblue hover:bg-blue text-xl py-2 rounded-md w-full ">
-      Create a Post!
-    </button>
-  );
-};
-
-export default PostButton;
