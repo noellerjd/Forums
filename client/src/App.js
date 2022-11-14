38019f248e278ca3ed7259127919d683d59d6748
dpvb <bungohdev@gmail.com>
2022-11-13 19:17:16 -0500

    Start implementing post page.

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import PostPage from "./pages/PostPage";
+import CreatePost from "./pages/CreatePost";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/post-page" element={<PostPage />} />
+          <Route path="/create-post" element={<CreatePost />} />
           <Route path="/" element={<Home />} />
         </Routes>
       </Router>
